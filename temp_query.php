00e1a4c532187a6a586bfab6870e94dc6d1593f0
DexsDevelopers <empresatokio@gmail.com>
2026-06-06 16:57:40 -0300

    debug: query server git status

--- temp_query.php
+++ temp_query.php
@@ -11,31 +11,15 @@ if (empty($_GET['token']) || $_GET['token'] !== 'diretopay_secure_debug_token_20
 try {
     $res = [];
     
-    // List files in the root directory
-    $files = scandir(__DIR__);
-    $res['root_files'] = [];
-    foreach ($files as $file) {
-        if ($file === '.' || $file === '..') continue;
-        $res['root_files'][] = [
-            'name' => $file,
-            'is_dir' => is_dir($file),
-            'size' => is_dir($file) ? 0 : filesize($file),
-            'mtime' => date('Y-m-d H:i:s', filemtime($file))
-        ];
-    }
+    // Check git log on server
+    $res['git_log'] = shell_exec('git log -n 5 --oneline 2>&1');
     
-    // Check if index.html exists and its content excerpt
-    if (file_exists('index.html')) {
-        $res['index_html_excerpt'] = substr(file_get_contents('index.html'), 0, 500);
-    } else {
-        $res['index_html_excerpt'] = null;
-    }
+    // Check git status on server
+    $res['git_status'] = shell_exec('git status 2>&1');
     
-    // Check index.php content excerpt
+    // Check what is inside index.php on the server
     if (file_exists('index.php')) {
-        $res['index_php_excerpt'] = substr(file_get_contents('index.php'), 0, 500);
-    } else {
-        $res['index_php_excerpt'] = null;
+        $res['index_php_content'] = file_get_contents('index.php');
     }
 
     echo json_encode(['success' => true, 'data' => $res]);
