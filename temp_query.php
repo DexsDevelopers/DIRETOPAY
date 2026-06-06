cfd0de4833156b2f931e2156d8d84db727348b1f
DexsDevelopers <empresatokio@gmail.com>
2026-06-06 16:57:57 -0300

    debug: query server htaccess

--- temp_query.php
+++ temp_query.php
@@ -11,15 +11,11 @@ if (empty($_GET['token']) || $_GET['token'] !== 'diretopay_secure_debug_token_20
 try {
     $res = [];
     
-    // Check git log on server
-    $res['git_log'] = shell_exec('git log -n 5 --oneline 2>&1');
-    
-    // Check git status on server
-    $res['git_status'] = shell_exec('git status 2>&1');
-    
-    // Check what is inside index.php on the server
-    if (file_exists('index.php')) {
-        $res['index_php_content'] = file_get_contents('index.php');
+    // Check .htaccess on server
+    if (file_exists('.htaccess')) {
+        $res['htaccess'] = file_get_contents('.htaccess');
+    } else {
+        $res['htaccess'] = null;
     }
 
     echo json_encode(['success' => true, 'data' => $res]);
